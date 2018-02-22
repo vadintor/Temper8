77554e188d8ab92febd481ec4efc9647d9acf482
Torbjörn Vading <torbjorn.vading@nivapro.com>
2018-02-22 23:30:11 +0100

    change router.get /api path

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -5,9 +5,7 @@ import express = require('express');
 const router: express.Router = express.Router();
 import Temper8 = require('../models/temper8');
 
-router.get('/api', (_req: express.Request, res: express.Response) => {
-    Temper8.Device.initlialize();
-
+router.get('/', (_req: express.Request, res: express.Response) => {
     const sensorData: Temper8.Sensor[] = Temper8.Device.getSensorData();
     res.setHeader('Content-Type', 'application/json');
     res.send(JSON.stringify(sensorData));
