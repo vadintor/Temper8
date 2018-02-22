5490d5f741e60b17441e7c1d171766761e468ee6
Torbjörn Vading <torbjorn.vading@nivapro.com>
2018-02-23 00:44:03 +0100

    add Access-Control-Allow-Origin

--- src/app.ts
+++ src/app.ts
@@ -21,12 +21,31 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', route_index);
 app.use('/api', route_api);
 
+app.use(function(req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4000');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+    // Pass to next layer of middleware
+    next();
+});
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
     const err: Error = new Error('Not Found');
     err['status'] = 404;
     next(err);
 });
+
 
 // error handlers
 
