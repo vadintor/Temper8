574402beda57e84b9e3713c2786ac939b5bdcf39
Torbjörn Vading <torbjorn.vading@nivapro.com>
2020-09-15 12:55:55 +0200

    Retrieve rssi and battery status for each ruuvi tag

--- src/features/ruuvi/ruuvi-tag.ts
+++ src/features/ruuvi/ruuvi-tag.ts
@@ -30,11 +30,10 @@ interface RuuviData2or4 {
     eddystoneId: string;
 }
 type RuuviData = RuuviData2or4 | RuuviData5;
-type Sensor  = {state: RuuviSensorState, log: SensorLog };
-interface Tag {
-    [index: string]: Sensor[];
-}
-interface PeripheralData {
+
+type Sensor  = { state: RuuviSensorState, log: SensorLog };
+
+export interface PeripheralData {
     id: string;
     address: string;
     addressType: string;
@@ -44,6 +43,22 @@ interface Peripheral extends PeripheralData {
     on(type: 'updated', listener: (data: RuuviData) => void): void;
     on(type: 'error', listener: (raw: unknown) => void): void;
 }
+export interface TagStatus {
+    dataFormat: number;
+    rssi: number;
+    battery: number;
+    txPower: number;
+    mac: string;
+}
+export interface Tag {
+    sensors: Sensor[];
+    data: PeripheralData;
+    status: TagStatus;
+}
+export interface Tags {
+    [index: string]: Tag;
+}
+
 export function init() {
     log.info('ruuvi.initRuuvi');
     ruuvi.on('found', (tag: Peripheral) => {
@@ -57,38 +72,19 @@ export function init() {
         log.error('ruuvi.ruuvi.on(error): ' + JSON.stringify(message));
     });
 }
-
-export function addSensor(tagID: string, category: Category) {
-    const found = tags[tagID].find((sensor) => sensor.state.getAttr().category === category);
-    if (!found && tags[tagID].length < 8) {
-        tags[tagID].push(newSensor(category));
-    }
-}
-export function removeSensor(tagID: string, category: Category) {
-    const found = tags[tagID].find((sensor) => sensor.state.getAttr().category === category);
-    if (found) {
-        const index = tags[tagID].indexOf(found);
-        tags[tagID].splice(index, 1);
-    }
-}
 export function getRuuviTags(): string[] {
     const ids: string[] = [];
     Object.keys(tags).map(key => ids.push(key));
     return ids;
 }
-export function getSensors(tagID: string): Category[] {
-    const sensors: Category[] = [];
-    tags[tagID].forEach(sensor => sensors.push(sensor.state.getAttr().category));
-    return sensors;
-}
 export function startLogging(tagID: string, category: Category) {
-    const found = tags[tagID].find((sensor) => sensor.state.getAttr().category === category);
+    const found = tags[tagID].sensors.find((sensor) => sensor.state.getAttr().category === category);
     if (found) {
         found.log.startLogging();
     }
 }
 export function StopLogging(tagID: string, category: Category) {
-    const found = tags[tagID].find((sensor) => sensor.state.getAttr().category === category);
+    const found = tags[tagID].sensors.find((sensor) => sensor.state.getAttr().category === category);
     if (found) {
         found.log.stopLogging();
     }
@@ -106,10 +102,61 @@ function newSensor(category: Category) {
     nextAvailablePort++;
     const state = new RuuviSensorState(attr, port);
     const log = new SensorLog(state, sensorLogService);
-    return {state, log};
+    return { state, log };
 }
-const tags: Tag = {};
-
+const tags: Tags = {};
+function createPeripheral(tag: Peripheral) {
+    const status: TagStatus = {dataFormat: 0, rssi: 0, battery: 0, txPower: 0, mac: tag.address};
+    tags[tag.id] = {
+        sensors: [ newSensor(Category.Temperature) ],
+        data: tag as PeripheralData,
+        status,
+    };
+    tag.on('updated', (raw: RuuviData)  => {
+        if (isRuuviData5Valid(raw)) {
+            const data = raw as RuuviData5;
+            tags[tag.id].status = {
+                dataFormat: data.dataFormat,
+                rssi: data.rssi,
+                battery: data.battery,
+                txPower: data.txPower,
+                mac: data.mac,
+            };
+            tags[tag.id].sensors.forEach((sensor) => {
+                switch (sensor.state.getAttr().category) {
+                    case Category.AbsoluteHumidity:
+                        sensor.state.update(data.humidity);
+                        break;
+                    case Category.AirPressure:
+                        sensor.state.update(data.pressure);
+                        break;
+                    case Category.AccelerationX:
+                        sensor.state.update(data.accelerationX);
+                        break;
+                    case Category.AccelerationY:
+                        sensor.state.update(data.accelerationY);
+                        break;
+                    case Category.AccelerationZ:
+                        sensor.state.update(data.accelerationZ);
+                        break;
+                    case Category.Humidity:
+                        sensor.state.update(data.humidity);
+                        break;
+                    case Category.RelativeHumidity:
+                        sensor.state.update(data.humidity);
+                        break;
+                    case Category.Temperature:
+                        sensor.state.update(data.temperature);
+                        break;
+                }
+            });
+        }
+    });
+    tag.on('error', (data: any)  => {
+        log.error('ruuvi-tag.createPeripheral.on(error): ' + JSON.stringify(data));
+    });
+    tags[tag.id].sensors.forEach(sensor => sensor.log.startLogging());
+}
 function isObject(raw: unknown) {
     return typeof raw === 'object' && raw !== null;
 }
@@ -121,56 +168,4 @@ function isRuuviData5Valid(raw: unknown) {
         && !!data.dataFormat && data.dataFormat === 5;
     }
     return valid;
-}
-
-function createPeripheral(tag: Peripheral) {
-    tags[tag.id] = [
-        newSensor(Category.Temperature),
-    ];
-    tag.on('updated', (raw: RuuviData)  => {
-        if (isRuuviData5Valid(raw)) {
-            const data = raw as RuuviData5;
-            tags[tag.id].forEach((sensor) => {
-                switch (sensor.state.getAttr().category) {
-                    case Category.AbsoluteHumidity:
-                        sensor.state.update(data.humidity);
-                        break;
-                    case Category.AbsoluteHumidity:
-                        sensor.state.update(data.humidity);
-                        break;
-                    case Category.AccelerationX:
-                        sensor.state.update(data.accelerationX);
-                        break;
-                    case Category.AccelerationY:
-                        sensor.state.update(data.accelerationY);
-                        break;
-                    case Category.AccelerationZ:
-                        sensor.state.update(data.accelerationZ);
-                        break;
-                    case Category.Battery:
-                        sensor.state.update(data.battery);
-                        break;
-                    case Category.Humidity:
-                        sensor.state.update(data.humidity);
-                        break;
-                    case Category.RelativeHumidity:
-                        sensor.state.update(data.humidity);
-                        break;
-                    case Category.Temperature:
-                        sensor.state.update(data.temperature);
-                        break;
-                    case Category.TxPower:
-                        sensor.state.update(data.txPower);
-                        break;
-                    case Category.rssi:
-                        sensor.state.update(data.rssi);
-                        break;
-                }
-            });
-        }
-    });
-    tag.on('error', (data: any)  => {
-        log.error('ruuvi-tag.createPeripheral.on(error): ' + JSON.stringify(data));
-    });
-    tags[tag.id].forEach(sensor => sensor.log.startLogging());
 }
